02a95f31a72c0f7e6ae8bf28c979ac1445e92e52
gthar <r.illa.pujagut@gmail.com>
2016-12-28 19:47:31 +0100

    key handler actions in the config file

--- src/rpi_wall/key_handler.clj
+++ src/rpi_wall/key_handler.clj
@@ -3,14 +3,15 @@
             [clojure.string     :refer [split]]
             [rpi-wall.helpers   :refer [config]]))
 
+(def actions
+  (:key-handlers config))
+
 (defn exec-str
   [x]
   (apply sh (split x #"\s+")))
 
 (defn key-handler
   [x]
-  (case x
-    "P" (println "this is P!")
-    "ArrowUp"   (exec-str (:screen-on-cmd  config))
-    "ArrowDown" (exec-str (:screen-off-cmd config))
+  (if-let [cmd (-> x keyword actions)]
+    (exec-str cmd)
     (println (str "no current binding for " x))))
