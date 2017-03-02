8fe999a71c176414a9e7a2ebcd81bf6533d830ce
gthar <r.illa.pujagut@gmail.com>
2017-03-02 17:46:40 +0100

    also limit list of events

--- src/rpi_wall/calendar/server.clj
+++ src/rpi_wall/calendar/server.clj
@@ -1,9 +1,11 @@
 (ns rpi-wall.calendar.server
   (:require [rpi-wall.calendar.busy-days  :refer [get-busy-days]]
             [rpi-wall.calendar.todo-today :refer [get-day-events]]
-            [rpi-wall.helpers             :refer [fetch-insist config counter]]))
+            [rpi-wall.helpers             :refer [fetch-insist config counter limiter]]))
 
 ;(google-apps-clj.credentials/get-auth-map creds ["https://www.googleapis.com/auth/calendar"])
+
+(def max-lines (:max-lines config))
 
 (def calendar-creds
   (:google-calendar config))
@@ -22,9 +24,18 @@
         callback #(f calendar-creds shift)]
     (fetch-insist callback ms)))
 
+(defn limit
+  [x n]
+  (if (> (count x) n)
+    (concat (take (dec n) x) [{:start nil :end nil :name "..."}])
+    x))
+
 (defn set-cal-info!
   []
   (reset! busy-days-state  (fetcher get-busy-days))
-  (reset! todo-today-state (counter (fetcher get-day-events))))
+  (reset! todo-today-state (->> get-day-events
+                                fetcher
+                                counter
+                                (limiter limit max-lines))))
 
 (set-cal-info!)
